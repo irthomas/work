4a02ccc71280f39c911ebde873e981ffe719dd35
Ian Thomas <ian.thomas@aeronomie.be>
2020-05-25 21:29:31 +0200

    Grazing occ search

---
+++ scripts/find_grazing_occultations.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 21:47:54 2020
+
+@author: iant
+
+find all grazing occultations
+
+"""
+
+#import os
+import re
+
+
+
+from tools.file.hdf5_functions import make_filelist
+
+
+#regex = re.compile(".*_SO_._G_.*")
+regex = re.compile(".*_UVIS_G")
+
+file_level = "hdf5_level_0p3k"
+
+_, hdf5_filenames, _ = make_filelist(regex, file_level, open_files=False)
+
+hdf5_filenames_sorted = sorted(hdf5_filenames)
+
+for hdf5_filename_sorted in hdf5_filenames_sorted:
+    print(hdf5_filename_sorted)
