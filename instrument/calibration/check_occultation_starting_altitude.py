bcaf34e16219bc8fd9bc06df10444905756fbc92
Ian Thomas <ian.thomas@aeronomie.be>
2020-12-03 20:51:41 +0100

    SO starting altitude and latitude vs time

--- instrument/calibration/check_occultation_starting_altitude.py
+++ instrument/calibration/check_occultation_starting_altitude.py
@@ -33,23 +33,40 @@ file_level = "hdf5_level_0p3k"
 
 datetime_strings = []
 max_alts = []
+lats = []
 
 hdf5_filepaths, hdf5_filenames, _ = make_filelist(regex, file_level, open_files=False)
 for hdf5_filepath, hdf5_filename in zip(hdf5_filepaths, hdf5_filenames):
     
     with h5py.File(hdf5_filepath, "r") as f:
-        alts = f["Geometry/Point0/TangentAlt"][:, 0]
+        alts = f["Geometry/Point0/TangentAlt"][0, 0]
+        latitudes_in = f["Geometry/Point0/Lat"][:, 0]
         
         datetime_in = f["Temperature/TemperatureDateTime"][...]
         
         if length(datetime_in) > 20:
 
-            max_alts.append(max(alts))
+            max_alts.append(alts)
 
             mid_point = int(len(datetime_in)/2)
             datetime_strings.append(datetime_in[mid_point].decode())
+
+        
+            mid_point = int(len(latitudes_in)/2)
+            lats.append(latitudes_in[mid_point])
+
         
 datetimes = [datetime.strptime(x, HDF5_DT_FORMAT) for x in datetime_strings]
 
-plt.figure()
-plt.plot(datetimes, max_alts)
+fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(18, 8), sharex=True)
+ax1.scatter(datetimes, lats, label="Ingress")
+ax1.set_ylabel("SO Midpoint Latitude")
+ax1.legend()
+
+ax2.plot(datetimes, max_alts, label="Ingress")
+ax2.set_xlabel("Time")
+ax2.set_ylabel("Starting Altitude")
+ax2.legend()
+ax2.grid()
+       
+plt.savefig("SO_starting_altitudes.png")
