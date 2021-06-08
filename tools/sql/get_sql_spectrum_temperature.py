66afa8344378e0bb217d76e0c9bc2cd4881e7714
Ian Thomas <ian.thomas@aeronomie.be>
2021-06-08 21:46:46 +0200

    comments added to SQL temperature functions

--- tools/sql/get_sql_spectrum_temperature.py
+++ tools/sql/get_sql_spectrum_temperature.py
@@ -8,11 +8,11 @@ from datetime import datetime, timedelta
 from scipy import interpolate
 
 from tools.general.get_nearest_datetime import get_nearest_datetime
-from tools.sql.heaters_temp import get_temperature_range
+from tools.sql.heaters_temp import get_temperature_range #taken from data pipeline
 
 
 def get_sql_spectrum_temperature(hdf5_file, frame_index, channel="lno"):
-    """get LNO nominal temperature readout closest to chosen frame in hdf5_file"""
+    """get nominal temperature readout closest to chosen frame in hdf5_file"""
     
     
     utc_start_time = hdf5_file["Geometry/ObservationDateTime"][0, 0].decode()
@@ -24,8 +24,9 @@ def get_sql_spectrum_temperature(hdf5_file, frame_index, channel="lno"):
     utc_obs_time = hdf5_file["Geometry/ObservationDateTime"][frame_index, 0].decode()
     utc_obs_datetime = datetime.strptime(utc_obs_time, "%Y %b %d %H:%M:%S.%f")
 
+    #get index
     obs_temperature_index = get_nearest_datetime([i[0] for i in temperatures], utc_obs_datetime)
-    #get LNO nominal temperature
+    #get column number for channel
     index = {"so":1, "lno":2, "uvis":3}[channel]
     
     measurement_temperature = float(temperatures[obs_temperature_index][index])
@@ -35,20 +36,26 @@ def get_sql_spectrum_temperature(hdf5_file, frame_index, channel="lno"):
 
 
 def get_sql_temperatures_all_spectra(hdf5_file, channel):
+    """read in a hdf5 file and return a temperature for each spectrum in the file, interpolated from TGO readouts"""
     
+    #get observation times from file
     utc_times = [i.decode() for i in hdf5_file["Geometry/ObservationDateTime"][:, 0]]
     utc_datetimes = [datetime.strptime(i, "%Y %b %d %H:%M:%S.%f") for i in utc_times]
     utc_start_datetime = utc_datetimes[0]
     utc_end_datetime = utc_datetimes[-1]
     
+    #convert to seconds
     frame_delta_seconds = [(i-utc_start_datetime).total_seconds() for i in utc_datetimes]
     
+    #get data from SQL for given range + 3 minutes
     temperature_data = get_temperature_range(utc_start_datetime - timedelta(minutes=3), utc_end_datetime + timedelta(minutes=3))
     temperature_delta_seconds = [(i[0]-utc_start_datetime).total_seconds() for i in temperature_data]
     
+    #choose channel
     index = {"so":1, "lno":2, "uvis":3}[channel]
     channel_temperatures = [float(i[index]) for i in temperature_data]
     
+    #interpolate onto observation times
     f = interpolate.interp1d(temperature_delta_seconds, channel_temperatures, kind="quadratic")
     
     frame_temperatures = f(frame_delta_seconds)
