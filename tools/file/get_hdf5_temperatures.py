9b4bb6aeac85c05645978c4e1029f75c927d930c
Ian Thomas <ian.thomas@aeronomie.be>
2021-08-25 15:31:04 +0200

    Tool to interpolate hdf5 temperatures added; asimut reader includes yerror and altitude info

---
+++ tools/file/get_hdf5_temperatures.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 4 14:54:31 2021
+
+@author: iant
+
+# example usage
+
+import h5py
+hdf5_file = h5py.File(r"<path_to_file>.h5", "r")
+so = get_interpolated_temperatures(hdf5_file, "so")
+lno = get_interpolated_temperatures(hdf5_file, "lno")
+uvis = get_interpolated_temperatures(hdf5_file, "uvis")
+
+"""
+
+
+from datetime import datetime
+from scipy import interpolate
+
+
+
+def get_interpolated_temperatures(hdf5_file, channel):
+    """give an open hdf5 file and return a temperature for each spectrum in the file, interpolated from TGO readouts"""
+    
+    #get observation times from file
+    utc_times = [i.decode() for i in hdf5_file["Geometry/ObservationDateTime"][:, 0]]
+    utc_datetimes = [datetime.strptime(i, "%Y %b %d %H:%M:%S.%f") for i in utc_times]
+    
+    
+    #get temperature times from file
+    utc_t_times = [i.decode() for i in hdf5_file["Temperature/TemperatureDateTime"][...]]
+    utc_t_datetimes = [datetime.strptime(i, "%Y %b %d %H:%M:%S.%f") for i in utc_t_times]
+    
+    
+    
+    #convert to seconds
+    utc_start_datetime = utc_datetimes[0]
+    frame_delta_seconds = [(i-utc_start_datetime).total_seconds() for i in utc_datetimes]
+    temperature_delta_seconds = [(i-utc_start_datetime).total_seconds() for i in utc_t_datetimes]
+    
+    #get channel dataset
+    channel_temperatures = hdf5_file["Temperature/Nominal%s" %channel.upper()][...]
+    
+    #interpolate onto observation times
+    f = interpolate.interp1d(temperature_delta_seconds, channel_temperatures, kind="quadratic")
+    
+    frame_temperatures = f(frame_delta_seconds)
+    return frame_temperatures
+
+
--- tools/file/read_asimut_output.py
+++ tools/file/read_asimut_output.py
@@ -70,6 +70,7 @@ for asi_name, dict_name in {"fileaotffilter":"aotf", "fileilsparam":"ils"}.items
         asi_info[dict_name] = dir_win                     
 
 asi_info["A_nu0"] = float(inp_config_dict["SP1"]["aotfcentralwnb"])
+asi_info["YError"] = inp_config_dict["SP1"]["datayerrorselect"]
 
 vals = inp_config_dict["SP1"]["spectraid_list"]
 vals = vals.replace("val[ ","").replace("]","").split(" ")
@@ -79,40 +80,63 @@ asi_info["out"] = os.path.join(asi_info["out_dir"], "%s_out.h5" %hdf5_filename)
 # plt.figure()
 #get data from output file
 with h5py.File(asi_info["out"], "r") as f:
+    
+    n = len(f.keys())
 
-    y_all = np.zeros((len(f.keys()), 320))
-    yr_all = np.zeros((len(f.keys()), 320))
+    y_all = np.zeros((n, 320))
+    yr_all = np.zeros((n, 320))
+    
+    # passes = sorted(list(f.keys()))
+    # pass_nos = sorted([int(s.strip("Pass_")) for s in passes])
+    # for pass_no in pass_nos:
+        
+    #     pass_name = "Pass_%i" %pass_no
+
     for key in f.keys():
-        ix = int(key.replace("Pass_","")) - 1
+        ix = int(key.replace("Pass_",""))
         y = f[key]["Fit_0"]["Y"][...]
         yr = f[key]["Fit_0"]["YCALC"][...]
-        
-        y_all[ix, :] = y
-        yr_all[ix, :] = yr
+        y_all[n - ix, :] = y
+        yr_all[n - ix, :] = yr
 
 
-# with h5py.File(asi_info["hdf5"], "r") as f2:
+with h5py.File(asi_info["hdf5"], "r") as f2:
     
-#     y_error = 
+    x = f2["Science/X"][0,:]
+    y_error_all = f2["Science/%s" %asi_info["YError"]][...]
+    alts = np.mean(f2["Geometry/Point0/TangentAltAreoid"][...], axis=1)
 
 # fig, ax = plt.subplots(figsize=(10,5))
 
 # fig = plt.figure(constrained_layout=True)
-fig = plt.figure(figsize=(10,5))
-gs = fig.add_gridspec(3, 1)
-ax1 = fig.add_subplot(gs[0:2, 0])
-ax2 = fig.add_subplot(gs[2, 0])
+fig = plt.figure(figsize=(10,8), constrained_layout=True)
+gs = fig.add_gridspec(2, 1)
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[1, 0], sharex=ax1)
+
+ax1.grid()
+ax2.grid()
 
 
-i = 109
-ax1.plot(y_all[i, :], label="Y")
-ax1.plot(yr_all[i, :], label="Y retrieved")
+i = 100
+i_l1 = asi_info["indices"][i]
+
+ax1.plot(x, y_all[i, :], label="Y")
+ax1.plot(x, yr_all[i, :], label="Y retrieved")
 
 residual = y_all[i, :] - yr_all[i, :]
-ax2.plot(residual, label="Residual")
+ax2.plot(x, residual, color="k", label="Residual")
+ax2.fill_between(x, y_error_all[i_l1, :], -y_error_all[i_l1, :], color="g", label=asi_info["YError"], alpha=0.3)
 
 ax2.set_xlabel("Pixel")
 ax1.set_ylabel("Transmittance")
 ax2.set_ylabel("Residual")
-ax1.set_title(hdf5_filename)
+ax1.set_title("%s: i=%i, %0.1fkm" %(hdf5_filename, i, alts[i_l1]))
+
+ax1.set_ylim((min(y_all[i, 20:])-0.01, max(y_all[i, 20:])+0.01))
+ax2.set_ylim((min(residual[20:])-0.003, max(residual[20:])+0.003))
+# ax2.set_ylim((-y_error_all[i_l1, 50], y_error_all[i_l1, 50]))
+
+ax1.legend()
+ax2.legend()
 fig.savefig("%s_retrieval.png" %hdf5_filename)
